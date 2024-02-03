094b4dd465b2302e3338b4ac7c0b5481482a0156
pravi <praveengraj11@gmail.com>
2024-02-03 09:56:49 +0530

    update

--- node.js
+++ node.js
@@ -1,16 +1,29 @@
+const bcrypt = require('bcrypt');
+
 exports.handler = async (event) => {
     const password = event.password;
-    const correctPassword = "ironman";
+    const correctPasswordHash = "$2b$10$UhV9P2XyZQ5F9wX1LndSHeG6VeCZlC5IEc2eWEps5rBuIYfWDXkHe";
 
-    if (password === correctPassword) {
+    try {
+        // Compare the entered password with the hashed password
+        const match = await bcrypt.compare(password, correctPasswordHash);
+
+        if (match) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ success: true }),
+            };
+        } else {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ success: false, error: "Incorrect password" }),
+            };
+        }
+    } catch (error) {
+        console.error("Error comparing passwords:", error);
         return {
-            statusCode: 200,
-            body: JSON.stringify({ success: true }),
-        };
-    } else {
-        return {
-            statusCode: 401,
-            body: JSON.stringify({ success: false, error: "Incorrect password" }),
+            statusCode: 500,
+            body: JSON.stringify({ success: false, error: "Internal server error" }),
         };
     }
 };
